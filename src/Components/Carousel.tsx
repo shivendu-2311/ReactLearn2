64ec3ef782e835401f296cbe379f26133bb68a9f
Shivendu Kumar <shivendusingh2002@gmail.com>
2022-07-31 10:51:31 +0530

    carousel Implemented

--- src/Components/Carousel.tsx
+++ src/Components/Carousel.tsx
@@ -8,7 +8,7 @@ const images = [
     "https://media.istockphoto.com/photos/carousel-picture-id146887434?b=1&k=20&m=146887434&s=170667a&w=0&h=rElurDmbyViNIP7AZMBUOaHqvlBdlB_A2JRZf9ZWNNA="
 ]
 return (<>
-<img src={images[current]}/>
+<img src={images[current]} alt="example"/>
 <button disabled={current===0? true: false} onClick={()=>setCurrent(current-1)}>leftt</button>
 <button disabled={current === images.length-1 ? true: false} onClick={() =>setCurrent(current+1)}>right</button>
 </>);
