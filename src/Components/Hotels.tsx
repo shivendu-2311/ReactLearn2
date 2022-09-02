868a44e9a48c36ac45104b592991059a29eefffd
Shivendu Kumar <shivendusingh2002@gmail.com>
2022-09-03 01:31:01 +0530

    UI backend integrated

--- src/Components/Hotels.tsx
+++ src/Components/Hotels.tsx
@@ -13,26 +13,24 @@ export default function Hotels(){
     const data = useSelector((x:AppState) => x.hotelSlice);
     useEffect(()=>{
         async function api(){
-
+          var myHeaders = new Headers();
+          myHeaders.append("Content-Type", "application/json");
+          myHeaders.append("Origin", "http://localhost:3005");
+          
+          var raw = JSON.stringify({
+            "query": "{\nhotell{\n  id, name,cuisines, featured_image\n}\n}\n"
+          });
+          
+          var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+           // redirect: 'follow'
+          };
             
-            var myHeaders = new Headers();
-           
-            myHeaders.append("Content-Type", "application/json");
-           
-            
-            var raw = JSON.stringify({
-              "query": "# Write your query or mutation here\n{\n  hotels {\n    id,name,cuisines,featured_image\n  }\n}\n"
-            });
-            
-            const requestOptions = {
-              method: 'POST',
-              headers: myHeaders,
-              body: raw,
-            //   redirect: 'follow'
-            };
-            const response = await fetch("https://graphqlnodegfg.herokuapp.com/graphql", requestOptions)
-            const json: {data: {hotels: IHotel[]}}= await response.json();
-            dispatch(setHotels(json.data.hotels.map(x => x)));
+            const response = await fetch("https://nodegfgfive.herokuapp.com/graphql", requestOptions)
+            const json: {data: {hotell: IHotel[]}}= await response.json();
+            dispatch(setHotels(json.data.hotell.map(x => x)));
         }
   api();
     },[dispatch]);
